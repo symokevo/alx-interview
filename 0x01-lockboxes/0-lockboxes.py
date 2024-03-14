d35594a4cf27a0424906b15ba8044cd2b5a36ed2
Simon Rabuogi <symokevo@gmail.com>
2024-03-14 21:10:40 +0300

    Correct lockboxes.py file

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""
-Method that determines if
-all the boxes can be opened
-"""
+'''A module for working with lockboxes.
+'''
 
 
 def canUnlockAll(boxes):
-    """
-    function canUnlockAll
-    """
-    size_box = len(boxes)
-    keys = [0]
-    
-    for key in keys:
-        for box in boxes[key]:
-            if box < size_box:
-                if box not in keys:
-                    keys.append(box)
-    
-    if size_box == len(keys):
-        return True
-    
-    return False
+    '''Checks if all the boxes in a list of boxes containing the keys
+    (indices) to other boxes can be unlocked given that the first
+    box is unlocked.
+    '''
+    n = len(boxes)
+    seen_boxes = set([0])
+    unseen_boxes = set(boxes[0]).difference(set([0]))
+    while len(unseen_boxes) > 0:
+        boxIdx = unseen_boxes.pop()
+        if not boxIdx or boxIdx >= n or boxIdx < 0:
+            continue
+        if boxIdx not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
+            seen_boxes.add(boxIdx)
+    return n == len(seen_boxes)
